672e7e909c60ede3b92683ecbdc2373730722bf2
MarwanNada <mamhan2020@gmail.com>
2025-12-16 01:45:31 +0200

    fix error

--- lib/layers.py
+++ lib/layers.py
@@ -15,7 +15,7 @@ class Layer:
         """
         raise NotImplementedError
 
-    def backward(self, output_gradient, learning_rate):
+    def backward(self, output_gradient):
         """
         Exception
         """
@@ -33,6 +33,9 @@ class Dense(Layer):
         
         # Initialize Biases: Zeros
         self.bias = np.zeros((1, output_size))
+        # New: Storage for gradients
+        self.grad_weights = None
+        self.grad_bias = None
 
     def forward(self, input_data):
         """
@@ -43,17 +46,21 @@ class Dense(Layer):
         self.output = np.dot(self.input, self.weights) + self.bias
         return self.output
 
-    def backward(self, output_gradient, learning_rate):
+    def backward(self, output_gradient):
         
         # 1. Calculate Gradients
         # dL/dW = X^T . dL/dY
-        weights_gradient = np.dot(self.input.T, output_gradient)
+        #weights_gradient = np.dot(self.input.T, output_gradient)
         
         # dL/dB = sum(dL/dY) 
-        bias_gradient = np.sum(output_gradient, axis=0, keepdims=True)
-        
+        #bias_gradient = np.sum(output_gradient, axis=0, keepdims=True)
+        # 1. Calculate Gradients
+        # We store them in 'self' so the Optimizer can find them later
+        self.grad_weights = np.dot(self.input.T, output_gradient)
+        self.grad_bias = np.sum(output_gradient, axis=0, keepdims=True)
         # 2. Calculate Input Gradient (to return to previous layer)
         # dL/dX = dL/dY . W^T
         input_gradient = np.dot(output_gradient, self.weights.T)
+
 
         return input_gradient
